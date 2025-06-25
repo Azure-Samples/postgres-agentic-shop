d6c27c74c05df98b5f06014d51d6846e129f8030
Rio Fujita <rifujita@microsoft.com>
2025-06-25 18:07:06 +0900

    Changed exported port

--- backend/startup.sh
+++ backend/startup.sh
@@ -9,7 +9,7 @@ if [ "$ENV" = "prod" ]; then
   echo "Starting in production mode..."
   uvicorn src.main:app \
     --host 0.0.0.0 \
-    --port 8000 \
+    --port 80 \
     --access-log \
     --log-config logging_config.yaml \
     --workers 8
@@ -17,7 +17,7 @@ else
   echo "Starting in development mode..."
   uvicorn src.main:app \
     --host 0.0.0.0 \
-    --port 8000 \
+    --port 80 \
     --reload \
     --access-log \
     --log-config logging_config.yaml
